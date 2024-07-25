6a24ea4f735b8ae4a80f1be45d781a56bd5abc5b
Raihan Romzi R <raihan.rakhman16@gmail.com>
2024-07-25 09:39:12 +0700

    finish add feature user input based on similarity score

--- src/pages/DietFood.jsx
+++ src/pages/DietFood.jsx
@@ -7,6 +7,20 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from '@/components/ui/Carousel.jsx';
+import {
+  setBreakfast,
+  setBreakfastCount,
+  setBreakfastMaximumCalories,
+  setBreakfastMinimumCalories,
+  setDinner,
+  setDinnerCount,
+  setDinnerMaximumCalories,
+  setDinnerMinimumCalories,
+  setLunch,
+  setLunchCount,
+  setLunchMaximumCalories,
+  setLunchMinimumCalories,
+} from '@/redux/reducer';
 import { Input } from '@/components/ui/Input.jsx';
 import { Label } from '@/components/ui/Label.jsx';
 import { useNavigate } from 'react-router-dom';
@@ -15,6 +29,11 @@ import Skeleton from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
 import * as Dialog from '@radix-ui/react-dialog';
 import { setSelectedDietFoods } from '@/redux/reducer';
+import * as Separator from '@radix-ui/react-separator';
+import * as Progress from '@radix-ui/react-progress';
+import '../scss/index.scss';
+import axios from 'axios';
+import { Info } from 'lucide-react';
 
 function DietFood() {
   const navigate = useNavigate();
@@ -37,6 +56,7 @@ function DietFood() {
     lunchMaximumCalories,
     dinnerMinimumCalories,
     dinnerMaximumCalories,
+    favoriteFoods,
   } = useSelector(state => state.user);
 
   const [loadingImages, setLoadingImages] = useState(true);
@@ -54,6 +74,12 @@ function DietFood() {
   const [selectedDinnerFoods, setSelectedDinnerFoods] = useState([]);
   const [dialogOpen, setDialogOpen] = useState(false);
   const [selectedFood, setSelectedFood] = useState(null);
+  const [highSimilarityFood, setHighSimilarityFood] = useState('');
+  const [lowSimilarityFood, setLowSimilarityFood] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [highSimilarityInfoOpen, setHighSimilarityInfoOpen] = useState(false);
+  const [lowSimilarityInfoOpen, setLowSimilarityInfoOpen] = useState(false);
 
   const handleFoodToggle = (food, type) => {
     const toggleFood = (selectedFoods, setSelectedFoods) => {
@@ -90,7 +116,7 @@ function DietFood() {
   const renderCarouselItems = (foods, type, selectedFoods) => {
     return foods.map((food, index) => {
       let imageUrl =
-        'https://www.nutritionfacts.org/wp-content/uploads/2019/08/default-image.jpg'; // Replace with an actual default image URL if needed
+        'https://www.nutritionfacts.org/wp-content/uploads/2019/08/default-image.jpg';
 
       try {
         // Parse ImagesClean to a valid JSON array
@@ -170,13 +196,105 @@ function DietFood() {
     navigate('/time-to-eat');
   };
 
+  const handleGenerateNewRecommendation = async () => {
+    setLoading(true);
+    setProgress(0);
+
+    const interval = setInterval(() => {
+      setProgress(prev => {
+        if (prev < 90) {
+          return prev + 1;
+        } else {
+          clearInterval(interval);
+          return prev;
+        }
+      });
+    }, 400);
+
+    try {
+      const response = await axios.post(
+        'http://103.217.145.107:8000/recommendations',
+        {
+          favoriteFoods,
+          tdee,
+          topNHigh: parseInt(highSimilarityFood),
+          topNLow: parseInt(lowSimilarityFood),
+        }
+      );
+
+      const { data } = response;
+
+      dispatch(setBreakfast(data.data.breakfast));
+      dispatch(setLunch(data.data.lunch));
+      dispatch(setDinner(data.data.dinner));
+      dispatch(setBreakfastCount(data.totalData.breakfast));
+      dispatch(setLunchCount(data.totalData.lunch));
+      dispatch(setDinnerCount(data.totalData.dinner));
+      dispatch(setBreakfastMinimumCalories(data.calories.breakfastMinimum));
+      dispatch(setLunchMinimumCalories(data.calories.lunchMinimum));
+      dispatch(setDinnerMinimumCalories(data.calories.dinnerMinimum));
+      dispatch(setBreakfastMaximumCalories(data.calories.breakfastMaximum));
+      dispatch(setLunchMaximumCalories(data.calories.lunchMaximum));
+      dispatch(setDinnerMaximumCalories(data.calories.dinnerMaximum));
+      setProgress(100);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setProgress(100);
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h2 className='text-3xl font-bold mb-4'>Choose Your Diet Food!</h2>
-      <p className={'font-sans text-sm font-medium'} style={{ color: '#000' }}>
+      <p
+        className={'font-sans text-sm font-medium mb-5'}
+        style={{ color: '#000' }}
+      >
         Please select foods for Breakfast, Dinner, and Lunch according to your
         calorie needs.
       </p>
+      <div className='form-responsive flex justify-end items-end mb-10'>
+        <div className='relative grid w-full items-center mr-6'>
+          <div className='flex justify-between items-center mb-2'>
+            <Label htmlFor='high-similarity'>High Similarity Score</Label>
+            <Info
+              className='h-5 w-5 ml-2 cursor-pointer'
+              onClick={() => setHighSimilarityInfoOpen(true)}
+            />
+          </div>
+          <Input
+            type='text'
+            id='high-similarity'
+            placeholder='80 (Default)'
+            value={highSimilarityFood}
+            onChange={e => setHighSimilarityFood(e.target.value)}
+            className='rounded-md p-2'
+          />
+        </div>
+        <div className='relative grid w-full items-center mr-6'>
+          <div className='flex justify-between items-center mb-2'>
+            <Label htmlFor='high-similarity'>Low Similarity Score</Label>
+            <Info
+              className='h-5 w-5 ml-2 cursor-pointer'
+              onClick={() => setLowSimilarityInfoOpen(true)}
+            />
+          </div>
+          <Input
+            type='text'
+            id='low-similarity'
+            placeholder='40 (Default)'
+            value={lowSimilarityFood}
+            onChange={e => setLowSimilarityFood(e.target.value)}
+            className='rounded-md p-2'
+          />
+        </div>
+        <Button variant='default' onClick={handleGenerateNewRecommendation}>
+          New Recommendation
+        </Button>
+      </div>
+      <Separator.Root className='SeparatorRoot' style={{ margin: '20px 0' }} />
       <div className='form-responsive mb-10'>
         <div className='grid mt-6 w-full items-start mr-6'>
           <div className='mb-4 text-xl font-semibold'>BMI Calculator</div>
@@ -221,7 +339,8 @@ function DietFood() {
           </div>
         </div>
       </div>
-      <div className='flex items-center'>
+
+      <div className='flex items-center mt-10'>
         <Label className={'font-sans text-2xl font-semibold'}>Breakfast</Label>
         <span className='ml-4 text-lg'>{breakfastCount} items</span>
         <span className='ml-4 text-lg'>
@@ -341,6 +460,74 @@ function DietFood() {
                   Instructions
                 </h3>
                 <p>{selectedFood?.RecipeInstructionsClean}</p>
+              </Dialog.Description>
+              <Dialog.Close asChild>
+                <Button variant='outline' className='mt-4'>
+                  Close
+                </Button>
+              </Dialog.Close>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+      <Dialog.Root open={loading}>
+        <Dialog.Overlay className='fixed inset-0 bg-black bg-opacity-50' />
+        <Dialog.Content className='fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-8 rounded-md w-96'>
+          <Dialog.Title className='text-lg font-bold'>Loading</Dialog.Title>
+          <Dialog.Description className='mt-2 mb-4'>
+            Please wait because currently we are preparing your food
+          </Dialog.Description>
+          <Progress.Root className='ProgressRoot' value={progress}>
+            <Progress.Indicator
+              className='ProgressIndicator'
+              style={{ transform: `translateX(-${100 - progress}%)` }}
+            />
+          </Progress.Root>
+        </Dialog.Content>
+      </Dialog.Root>
+      <Dialog.Root
+        open={highSimilarityInfoOpen}
+        onOpenChange={setHighSimilarityInfoOpen}
+      >
+        <Dialog.Portal>
+          <Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
+          <Dialog.Content className='fixed inset-0 flex items-center justify-center p-4'>
+            <div className='bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+              <Dialog.Title className='text-2xl font-bold mb-2'>
+                High Similarity Foods
+              </Dialog.Title>
+              <Dialog.Description className='mt-2 text-sm'>
+                High similarity foods are those that have a similar nutritional
+                profile or ingredients to the foods you have chosen as your
+                favorites. They are recommended to help you achieve your dietary
+                goals while providing variety in your meal plan.
+              </Dialog.Description>
+              <Dialog.Close asChild>
+                <Button variant='outline' className='mt-4'>
+                  Close
+                </Button>
+              </Dialog.Close>
+            </div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+
+      <Dialog.Root
+        open={lowSimilarityInfoOpen}
+        onOpenChange={setLowSimilarityInfoOpen}
+      >
+        <Dialog.Portal>
+          <Dialog.Overlay className='fixed inset-0 bg-black opacity-30' />
+          <Dialog.Content className='fixed inset-0 flex items-center justify-center p-4'>
+            <div className='bg-white p-6 rounded-lg shadow-lg max-w-lg w-full'>
+              <Dialog.Title className='text-2xl font-bold mb-2'>
+                Low Similarity Foods
+              </Dialog.Title>
+              <Dialog.Description className='mt-2 text-sm'>
+                Low similarity foods are those that have not very similar
+                nutritional profiles or ingredients compared to the foods you
+                have chosen as your favorites. They provide variety in your meal
+                plan while helping you achieve your dietary goals.
               </Dialog.Description>
               <Dialog.Close asChild>
                 <Button variant='outline' className='mt-4'>
